5156eab354ea639a4fcb9c1b864bafcd31f426a2
johnnyeven_work <johnnyeven@gmail.com>
2014-08-05 15:08:53 +0800

    1

---
+++ code/proj.android/obj/local/armeabi/objs-debug/cocos2dx_static/__/external/unzip/unzip.o.d
@@ -0,0 +1,152 @@
+./obj/local/armeabi/objs-debug/cocos2dx_static/__/external/unzip/unzip.o: \
+  jni/../../cocos2d/cocos/./../external/unzip/unzip.cpp \
+  E:/android-ndk-r9d/sources/cxx-stl/llvm-libc++/../../android/support/include\stdio.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\stdio.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\sys/cdefs.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\sys/cdefs_elf.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\android/api-level.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\sys/types.h \
+  E:/android-ndk-r9d/sources/cxx-stl/llvm-libc++/../../android/support/include\stdint.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\stdint.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\sys/_types.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\machine/_types.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\limits.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\sys/limits.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\linux/limits.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\machine/internal_types.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\machine/limits.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\sys/syslimits.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\asm/page.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\linux/posix_types.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\linux/stddef.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\linux/compiler.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\asm/posix_types.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\asm/types.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\linux/types.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\machine/kernel.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\sys/sysmacros.h \
+  E:/android-ndk-r9d/sources/cxx-stl/llvm-libc++/../../android/support/include\wchar.h \
+  E:/android-ndk-r9d/sources/cxx-stl/llvm-libc++/../../android/support/include\wctype.h \
+  E:/android-ndk-r9d/sources/cxx-stl/llvm-libc++/../../android/support/include\xlocale.h \
+  E:/android-ndk-r9d/sources/cxx-stl/llvm-libc++/../../android/support/include\stdlib.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\stdlib.h \
+  E:/android-ndk-r9d/sources/cxx-stl/llvm-libc++/../../android/support/include\string.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\string.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\malloc.h \
+  E:/android-ndk-r9d/sources/cxx-stl/llvm-libc++/../../android/support/include\monetary.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\alloca.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\strings.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\memory.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\zlib.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include/zconf.h \
+  jni/../../cocos2d/cocos/./../external/unzip/unzip.h \
+  jni/../../cocos2d/cocos/./platform/android\CCPlatformDefine.h \
+  jni/../../cocos2d/cocos/.\base/CCPlatformConfig.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\android/log.h \
+  jni/../../cocos2d/cocos/./../external/unzip/ioapi.h \
+  E:/android-ndk-r9d/sources/cxx-stl/llvm-libc++/../../android/support/include\errno.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\errno.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\linux/errno.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\asm/errno.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\asm-generic/errno.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\asm-generic/errno-base.h
+
+E:/android-ndk-r9d/sources/cxx-stl/llvm-libc++/../../android/support/include\stdio.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\stdio.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\sys/cdefs.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\sys/cdefs_elf.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\android/api-level.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\sys/types.h:
+
+E:/android-ndk-r9d/sources/cxx-stl/llvm-libc++/../../android/support/include\stdint.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\stdint.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\sys/_types.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\machine/_types.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\limits.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\sys/limits.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\linux/limits.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\machine/internal_types.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\machine/limits.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\sys/syslimits.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\asm/page.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\linux/posix_types.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\linux/stddef.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\linux/compiler.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\asm/posix_types.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\asm/types.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\linux/types.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\machine/kernel.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\sys/sysmacros.h:
+
+E:/android-ndk-r9d/sources/cxx-stl/llvm-libc++/../../android/support/include\wchar.h:
+
+E:/android-ndk-r9d/sources/cxx-stl/llvm-libc++/../../android/support/include\wctype.h:
+
+E:/android-ndk-r9d/sources/cxx-stl/llvm-libc++/../../android/support/include\xlocale.h:
+
+E:/android-ndk-r9d/sources/cxx-stl/llvm-libc++/../../android/support/include\stdlib.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\stdlib.h:
+
+E:/android-ndk-r9d/sources/cxx-stl/llvm-libc++/../../android/support/include\string.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\string.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\malloc.h:
+
+E:/android-ndk-r9d/sources/cxx-stl/llvm-libc++/../../android/support/include\monetary.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\alloca.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\strings.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\memory.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\zlib.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include/zconf.h:
+
+jni/../../cocos2d/cocos/./../external/unzip/unzip.h:
+
+jni/../../cocos2d/cocos/./platform/android\CCPlatformDefine.h:
+
+jni/../../cocos2d/cocos/.\base/CCPlatformConfig.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\android/log.h:
+
+jni/../../cocos2d/cocos/./../external/unzip/ioapi.h:
+
+E:/android-ndk-r9d/sources/cxx-stl/llvm-libc++/../../android/support/include\errno.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\errno.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\linux/errno.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\asm/errno.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\asm-generic/errno.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\asm-generic/errno-base.h:
---
+++ code/proj.android/obj/local/armeabi/objs-debug/cocos2dx_static/physics/chipmunk/CCPhysicsBodyInfo_chipmunk.o.d
@@ -0,0 +1,191 @@
+./obj/local/armeabi/objs-debug/cocos2dx_static/physics/chipmunk/CCPhysicsBodyInfo_chipmunk.o: \
+  jni/../../cocos2d/cocos/./physics/chipmunk/CCPhysicsBodyInfo_chipmunk.cpp \
+  jni/../../cocos2d/cocos/./physics/chipmunk/CCPhysicsBodyInfo_chipmunk.h \
+  jni/../../cocos2d/cocos/.\base/ccConfig.h \
+  jni/../../cocos2d/cocos/.\base/CCPlatformConfig.h \
+  jni/../../cocos2d/cocos/./../external/chipmunk/include/chipmunk\chipmunk.h \
+  E:/android-ndk-r9d/sources/cxx-stl/llvm-libc++/../../android/support/include\stdlib.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\stdlib.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\sys/cdefs.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\sys/cdefs_elf.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\android/api-level.h \
+  E:/android-ndk-r9d/sources/cxx-stl/llvm-libc++/../../android/support/include\string.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\string.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\malloc.h \
+  E:/android-ndk-r9d/sources/cxx-stl/llvm-libc++/../../android/support/include\monetary.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\sys/types.h \
+  E:/android-ndk-r9d/sources/cxx-stl/llvm-libc++/../../android/support/include\stdint.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\stdint.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\sys/_types.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\machine/_types.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\limits.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\sys/limits.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\linux/limits.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\machine/internal_types.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\machine/limits.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\sys/syslimits.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\asm/page.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\linux/posix_types.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\linux/stddef.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\linux/compiler.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\asm/posix_types.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\asm/types.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\linux/types.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\machine/kernel.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\sys/sysmacros.h \
+  E:/android-ndk-r9d/sources/cxx-stl/llvm-libc++/../../android/support/include\xlocale.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\alloca.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\strings.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\memory.h \
+  E:/android-ndk-r9d/sources/cxx-stl/llvm-libc++/../../android/support/include\math.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\math.h \
+  jni/../../cocos2d/cocos/./../external/chipmunk/include/chipmunk/chipmunk_types.h \
+  jni/../../cocos2d/cocos/./../external/chipmunk/include/chipmunk/cpVect.h \
+  jni/../../cocos2d/cocos/./../external/chipmunk/include/chipmunk/cpBB.h \
+  jni/../../cocos2d/cocos/./../external/chipmunk/include/chipmunk/cpSpatialIndex.h \
+  jni/../../cocos2d/cocos/./../external/chipmunk/include/chipmunk/cpBody.h \
+  jni/../../cocos2d/cocos/./../external/chipmunk/include/chipmunk/cpShape.h \
+  jni/../../cocos2d/cocos/./../external/chipmunk/include/chipmunk/cpPolyShape.h \
+  jni/../../cocos2d/cocos/./../external/chipmunk/include/chipmunk/cpArbiter.h \
+  jni/../../cocos2d/cocos/./../external/chipmunk/include/chipmunk/constraints/cpConstraint.h \
+  jni/../../cocos2d/cocos/./../external/chipmunk/include/chipmunk/constraints/cpPinJoint.h \
+  jni/../../cocos2d/cocos/./../external/chipmunk/include/chipmunk/constraints/cpSlideJoint.h \
+  jni/../../cocos2d/cocos/./../external/chipmunk/include/chipmunk/constraints/cpPivotJoint.h \
+  jni/../../cocos2d/cocos/./../external/chipmunk/include/chipmunk/constraints/cpGrooveJoint.h \
+  jni/../../cocos2d/cocos/./../external/chipmunk/include/chipmunk/constraints/cpDampedSpring.h \
+  jni/../../cocos2d/cocos/./../external/chipmunk/include/chipmunk/constraints/cpDampedRotarySpring.h \
+  jni/../../cocos2d/cocos/./../external/chipmunk/include/chipmunk/constraints/cpRotaryLimitJoint.h \
+  jni/../../cocos2d/cocos/./../external/chipmunk/include/chipmunk/constraints/cpRatchetJoint.h \
+  jni/../../cocos2d/cocos/./../external/chipmunk/include/chipmunk/constraints/cpGearJoint.h \
+  jni/../../cocos2d/cocos/./../external/chipmunk/include/chipmunk/constraints/cpSimpleMotor.h \
+  jni/../../cocos2d/cocos/./../external/chipmunk/include/chipmunk/cpSpace.h \
+  jni/../../cocos2d/cocos/.\base/CCPlatformMacros.h \
+  jni/../../cocos2d/cocos/./platform/android\CCPlatformDefine.h \
+  E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\android/log.h \
+  jni/../../cocos2d/cocos/.\base/CCRef.h
+
+jni/../../cocos2d/cocos/./physics/chipmunk/CCPhysicsBodyInfo_chipmunk.h:
+
+jni/../../cocos2d/cocos/.\base/ccConfig.h:
+
+jni/../../cocos2d/cocos/.\base/CCPlatformConfig.h:
+
+jni/../../cocos2d/cocos/./../external/chipmunk/include/chipmunk\chipmunk.h:
+
+E:/android-ndk-r9d/sources/cxx-stl/llvm-libc++/../../android/support/include\stdlib.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\stdlib.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\sys/cdefs.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\sys/cdefs_elf.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\android/api-level.h:
+
+E:/android-ndk-r9d/sources/cxx-stl/llvm-libc++/../../android/support/include\string.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\string.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\malloc.h:
+
+E:/android-ndk-r9d/sources/cxx-stl/llvm-libc++/../../android/support/include\monetary.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\sys/types.h:
+
+E:/android-ndk-r9d/sources/cxx-stl/llvm-libc++/../../android/support/include\stdint.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\stdint.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\sys/_types.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\machine/_types.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\limits.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\sys/limits.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\linux/limits.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\machine/internal_types.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\machine/limits.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\sys/syslimits.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\asm/page.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\linux/posix_types.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\linux/stddef.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\linux/compiler.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\asm/posix_types.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\asm/types.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\linux/types.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\machine/kernel.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\sys/sysmacros.h:
+
+E:/android-ndk-r9d/sources/cxx-stl/llvm-libc++/../../android/support/include\xlocale.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\alloca.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\strings.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\memory.h:
+
+E:/android-ndk-r9d/sources/cxx-stl/llvm-libc++/../../android/support/include\math.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\math.h:
+
+jni/../../cocos2d/cocos/./../external/chipmunk/include/chipmunk/chipmunk_types.h:
+
+jni/../../cocos2d/cocos/./../external/chipmunk/include/chipmunk/cpVect.h:
+
+jni/../../cocos2d/cocos/./../external/chipmunk/include/chipmunk/cpBB.h:
+
+jni/../../cocos2d/cocos/./../external/chipmunk/include/chipmunk/cpSpatialIndex.h:
+
+jni/../../cocos2d/cocos/./../external/chipmunk/include/chipmunk/cpBody.h:
+
+jni/../../cocos2d/cocos/./../external/chipmunk/include/chipmunk/cpShape.h:
+
+jni/../../cocos2d/cocos/./../external/chipmunk/include/chipmunk/cpPolyShape.h:
+
+jni/../../cocos2d/cocos/./../external/chipmunk/include/chipmunk/cpArbiter.h:
+
+jni/../../cocos2d/cocos/./../external/chipmunk/include/chipmunk/constraints/cpConstraint.h:
+
+jni/../../cocos2d/cocos/./../external/chipmunk/include/chipmunk/constraints/cpPinJoint.h:
+
+jni/../../cocos2d/cocos/./../external/chipmunk/include/chipmunk/constraints/cpSlideJoint.h:
+
+jni/../../cocos2d/cocos/./../external/chipmunk/include/chipmunk/constraints/cpPivotJoint.h:
+
+jni/../../cocos2d/cocos/./../external/chipmunk/include/chipmunk/constraints/cpGrooveJoint.h:
+
+jni/../../cocos2d/cocos/./../external/chipmunk/include/chipmunk/constraints/cpDampedSpring.h:
+
+jni/../../cocos2d/cocos/./../external/chipmunk/include/chipmunk/constraints/cpDampedRotarySpring.h:
+
+jni/../../cocos2d/cocos/./../external/chipmunk/include/chipmunk/constraints/cpRotaryLimitJoint.h:
+
+jni/../../cocos2d/cocos/./../external/chipmunk/include/chipmunk/constraints/cpRatchetJoint.h:
+
+jni/../../cocos2d/cocos/./../external/chipmunk/include/chipmunk/constraints/cpGearJoint.h:
+
+jni/../../cocos2d/cocos/./../external/chipmunk/include/chipmunk/constraints/cpSimpleMotor.h:
+
+jni/../../cocos2d/cocos/./../external/chipmunk/include/chipmunk/cpSpace.h:
+
+jni/../../cocos2d/cocos/.\base/CCPlatformMacros.h:
+
+jni/../../cocos2d/cocos/./platform/android\CCPlatformDefine.h:
+
+E:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include\android/log.h:
+
+jni/../../cocos2d/cocos/.\base/CCRef.h:
